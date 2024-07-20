595ba356b2670177f4b0f3069cd92f30e848dbc6
Jayanth2211 <jayanthgama2001@gmail.com>
2024-07-20 23:06:38 +0530

    update

--- hotstar/src/App.js
+++ hotstar/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Home from './components/home';
 import Movies from './components/movies';
 import Navbar from './components/navbar';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Tv from './components/tv';
 import Sports from './components/sports';
 import Search from './components/search';
@@ -16,24 +16,24 @@ import SignUp from './components/signin';
 function App() {
   return (
     <div className="App">
-      
+
       <BrowserRouter>
-        <Navbar/>
-    
-      <Routes>
-      <Route path='/' element={<Login/>}/>
-      <Route path='/signUp' element={<SignUp/>}/>
-        <Route path='/home' element={<Home/>}/>
-        <Route path='/watchlist' element={<WatchList/>}/>
-        <Route path='/search' element={<Search/>}/>
-        <Route path='/movies' element={<Movies/>}/>
-        <Route path='/tv' element={<Tv/>}/>
-        <Route path='/sports' element={<Sports/>}/>
-        <Route path='/singlePage/:id' element={<SinglePage/>}/>
-        
-      </Routes>
+        <Navbar />
+
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/signUp' element={<SignUp />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/watchlist' element={<WatchList />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/movies' element={<Movies />} />
+          <Route path='/tv' element={<Tv />} />
+          <Route path='/sports' element={<Sports />} />
+          <Route path='/singlePage/:id' element={<SinglePage />} />
+
+        </Routes>
       </BrowserRouter>
-      
+
     </div>
   );
 }
