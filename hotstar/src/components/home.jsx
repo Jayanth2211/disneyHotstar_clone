0e8e68699d29585858dd7934a2daefd1cbc4dcb2
Jayanth2211 <jayanthgama2001@gmail.com>
2024-08-01 22:25:35 +0530

    update

--- hotstar/src/components/home.jsx
+++ hotstar/src/components/home.jsx
@@ -76,7 +76,6 @@ let Home=()=>{
     return(
         <div className="home">
          
-          
             <div className="ads">
                 <div className="adsImage">
                    <img src='https://shifu.hotstarext.com/SOURCE/VOD/cd-2023-08-17/Chandrayaan3CS_deskXbb-d88b18d7-cdc5-4371-a822-33a198ad0d47.jpg' alt="" />
